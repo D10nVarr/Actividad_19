a6ef02743ee8c2408296b0194defca7cb5ab8629
diegobarr <d1390xd.v@gmail.com>
2025-08-13 17:55:56 -0600

    Clase padre inicial

---
+++ jerarquiaGalletas.py
@@ -0,0 +1,9 @@
+class Galleta:
+    def __init__(self, nombre, precio, peso):
+        self.nombre = nombre
+        self.precio = precio
+        self.peso = peso
+
+    def mostrar_galleta(self):
+        print(f"Nombre: {self.nombre}, Precio: {self.precio}, Peso: {self.peso}")
+
